597be28ec2dee162f072008137ed2206f0f010ba
Luis <codazongroup@gmail.com>
2016-04-19 19:01:32 +0700

    Luis is fix bug Home 3, 4 - Update ajaxcart

--- skin/frontend/codazon_fastest/default/ajaxcart/ajaxcart.js
+++ skin/frontend/codazon_fastest/default/ajaxcart/ajaxcart.js
@@ -29,6 +29,7 @@ var ajaxcart = {
                         });
                     },
                     onSuccess	: function(response) {
+						jQuery('body').trigger('ajaxCartCompleted');
                         // Handle the response content...
                         try{
                             var res = response.responseText.evalJSON();
@@ -194,6 +195,7 @@ var ajaxcart = {
                 });
             },
             onSuccess	: function(response) {
+				jQuery('body').trigger('ajaxCartCompleted');
                 // Handle the response content...
                 try{
                     var res = response.responseText.evalJSON();
